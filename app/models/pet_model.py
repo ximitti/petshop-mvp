078eb471231e1570f59563a016c0aed211732f5b
leonardo perrella <leonardo.perrella@yahoo.com.br>
2021-07-12 20:35:49 -0300

    feat: Add PetModel

---
+++ app/models/pet_model.py
@@ -0,0 +1,31 @@
+from . import db
+from sqlalchemy import Column, String, Integer, Text, ForeignKey
+
+
+class PetModel(db.Model):
+    __tablename__ = "pets"
+
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String(50), nullable=False)
+    species = Column(String(50), nullable=False)
+    size = Column(String(50), nullable=False)
+    allergy = Column(String(50), nullable=False)
+    breed = Column(String(50))
+    fur = Column(String(50))
+    photo_url = Column(Text())
+
+    client_id = Column(Integer, ForeignKey("client.id"), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "species": self.species,
+            "allergy": self.allergy,
+            "breed": self.breed,
+            "fur": self.fur,
+            "photo_url": self.photo_url,
+            "client_id": self.client_id,
+        }
