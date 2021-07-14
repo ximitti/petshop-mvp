df9af2f9d7eda60d7976f3cb91cdc9900673e28b
leonardo perrella <leonardo.perrella@yahoo.com.br>
2021-07-13 22:17:33 -0300

    fix: id_admin to is_admin

--- app/models/petshop_model.py
+++ app/models/petshop_model.py
@@ -1,9 +1,16 @@
 from . import db
 from sqlalchemy import Column, String, Integer, Boolean
 from werkzeug.security import generate_password_hash, check_password_hash
+from dataclasses import dataclass
 
 
+@dataclass
 class PetshopModel(db.Model):
+    id: int
+    name: str
+    email: str
+    is_admin: bool
+
     __tablename__ = "petshops"
 
     id = Column(Integer, primary_key=True)
@@ -11,7 +18,7 @@ class PetshopModel(db.Model):
     name = Column(String(150), nullable=False)
     email = Column(String(150), nullable=False, unique=True)
     password_hash = Column(String(150), nullable=False)
-    id_admin = Column(Boolean(), default=True)
+    is_admin = Column(Boolean(), default=True)
 
     @property
     def password(self):
@@ -30,5 +37,5 @@ class PetshopModel(db.Model):
             "id": self.id,
             "name": self.name,
             "email": self.email,
-            "id_admin": self.id_admin,
+            "is_admin": self.is_admin,
         }
