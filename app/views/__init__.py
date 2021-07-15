8ae4a6406afb7f8a414c65a69401d80064927b21
Alex Schmitt <alex.schmitt@outlook.com>
2021-07-15 19:00:19 -0300

    feat: created order view and routes

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -5,8 +5,13 @@ def init_app(app: Flask):
     # TODO from .your_view import bp as bp_something
     # TODO app.register_blueprint(bp_something)
     from .client_view import bp as bp_client
+
     app.register_blueprint(bp_client)
 
-
     from .petshop_view import bp as bp_petshop
+
     app.register_blueprint(bp_petshop)
+
+    from .order_view import bp as bp_order
+
+    app.register_blueprint(bp_order)
