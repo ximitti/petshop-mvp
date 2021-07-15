a4a964188f09bc7c6d3c8f20d0a9d8ce02a002fe
João Pedro A <jaaopbr@gmail.com>
2021-07-15 16:17:38 -0300

    feat: finished basic crud

--- app/views/pet_view.py
+++ app/views/pet_view.py
@@ -1,4 +1,5 @@
 from app.models.pet_model import PetModel
+from app.models.order_model import OrderModel
 from flask import Blueprint, jsonify, current_app, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from http import HTTPStatus
@@ -11,16 +12,19 @@ bp = Blueprint("bp_pet", __name__, url_prefix="/api")
 def create():
     session = current_app.db.session
     data = request.get_json()
+    client_id = data['client_id']
+    pet_owner_id = get_jwt_identity()
 
-    try:
-        pet: PetModel = PetModel(**data)
-        session.add(pet)
-        session.commit()
-    except:
-        return {"error": "Internal Error"}, HTTPStatus.INTERNAL_SERVER_ERROR
-
-    return {"message": "Pet created", "data": pet.serialize}, HTTPStatus.CREATED
-
+    try:   
+        if client_id == pet_owner_id:
+            pet: PetModel = PetModel(**data)
+            session.add(pet)
+            session.commit()
+            return {"message": "Pet created", "data": pet.serialize}, HTTPStatus.CREATED
+        else:
+            raise KeyError  
+    except KeyError as _:
+        return {"error": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
 
 @bp.get('/pets/')
 @jwt_required()
@@ -45,13 +49,14 @@ def retrieve_all():
 def retrieve_by_id(pet_id: int):
     try:
         pet: PetModel = PetModel.query.get(pet_id)
-    except:
-        return {"error": "Internal Error"}, HTTPStatus.INTERNAL_SERVER_ERROR
 
-    if not pet:
+        if not pet:
+            raise KeyError
+        else:
+            return {"data": pet.serialize}, HTTPStatus.OK
+    except KeyError as _:
         return {"error": "Pet not found"}, HTTPStatus.NOT_FOUND
-    
-    return {"data": pet.serialize}, HTTPStatus.OK
+
 
 @bp.patch('/pets/<int:pet_id>')
 @jwt_required()
@@ -65,7 +70,7 @@ def update(pet_id: int):
         session.commit()
         return {"data": pet.serialize}, HTTPStatus.OK
     except KeyError:
-        return {"error": ""}
+        return {"error": "Internal Error"}, HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 @bp.delete('/pets/<int:pet_id>')
@@ -86,3 +91,23 @@ def delete(pet_id: int):
 
     except KeyError as _:
         return {"error": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
+
+
+@bp.get('/pets/<int:pet_id>/orders')
+@jwt_required()
+def get_orders_by_pet(pet_id: int):
+    pet_owner_id = get_jwt_identity()
+
+    try:
+        pet: PetModel = PetModel.query.get(pet_id)
+        if not pet:
+            raise KeyError
+
+        if pet.client_id == pet_owner_id:
+            orders: OrderModel = OrderModel.query.filter_by(pet_id=pet_id).all()
+            orders = [order.serialize for order in orders]
+            return {"data": orders}, HTTPStatus.OK
+        else:
+            return {"error": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
+    except KeyError as _:
+        return {"error": "Pet not found"}, HTTPStatus.NOT_FOUND
