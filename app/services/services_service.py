e0a30f1e101cfcce7722e04c968cb44b0da98261
Lucas Passeli <lucaspasseli.work@gmail.com>
2021-07-20 19:53:09 -0300

    ♻️ refactor:  refactor services view

--- app/services/services_service.py
+++ app/services/services_service.py
@@ -1,6 +1,11 @@
 from app.models.services_model import ServicesModel
-from flask import current_app, jsonify
+from app.exc.status_not_found import NotFoundError
+from app.exc.status_option import InvalidKeysError
 from http import HTTPStatus
+from flask import current_app
+from flask_jwt_extended import (
+    get_jwt_identity,
+)
 
 def check_valid_keys(data, valid_keys, key):
     if key not in valid_keys:
@@ -48,4 +53,17 @@ def update_service(data, service_id: int):
              raise NotFoundError("Service not found")
         return service
     return HTTPStatus.UNAUTHORIZED
+
+def delete_service(service_id: int):
+    session = current_app.db.session
+    service = ServicesModel.query.get(service_id)    
+    if not service:
+            raise NotFoundError("Service not found")
+
+    service: ServicesModel = ServicesModel.query.get(service_id)
+    
+    session.delete(service)
+    session.commit()
+
+    return ""
 
--- app/views/services_view.py
+++ app/views/services_view.py
@@ -1,25 +1,24 @@
 from http import HTTPStatus
 from app.exc.status_option import InvalidKeysError
+from app.exc.status_not_found import NotFoundError
 from app.models.services_model import ServicesModel
-from flask import Blueprint, json, jsonify,current_app, request
-from flask_jwt_extended import (get_jwt_identity,
-    jwt_required,
-)
-from app.services.services_service import (get_services, create_service, get_service_by_id, update_service)
+from flask import Blueprint, json, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt
+from app.services.helpers import is_admin
+from app.services.services_service import (delete_service, get_services, create_service, get_service_by_id, update_service)
 
 bp  = Blueprint("bp", __name__, url_prefix="/services")
 
 @bp.get("/")
 def get():
-    try:
-        return jsonify(data=services), HTTPStatus.OK
-    except NotFoundError as e:
-        return jsonify(e.message), HTTPStatus.NOT_FOUND
+    services = get_services()
+    return jsonify({"data": services}), HTTPStatus.OK
 
 @bp.post("/register")
 @jwt_required()
 def create():
     try:
+        is_admin(get_jwt())
         data = request.get_json()
         service = create_service(data)
         return json(data=service.serialize), HTTPStatus.CREATED
@@ -29,6 +28,7 @@ def create():
 @bp.get("/<int:id>")
 def retrieve_by_id(service_id):
     try:
+        is_admin(get_jwt())
         service = get_service_by_id(service_id)
         return json(data=service.serialize), HTTPStatus.OK
     except NotFoundError as e:
@@ -36,34 +36,21 @@ def retrieve_by_id(service_id):
 
 @bp.patch("/<int:id>")
 @jwt_required()
-def update_service_by_id(id):
-    session = current_app.db.session
-    current_service_id = get_jwt_identity()
+def update_service_by_id(service_id):
     data = request.get_json()
-    if current_service_id == id:
-        try:
-            service = ServicesModel.query.get(current_service_id)
-            service.name = data["name"]
-            session.commit()
-            return jsonify(service.serialize)
-        except KeyError:
-            return {"message": "avaiable keys : name"}, 404
-        except:
-            return {"messege":"Not Found"}
-    return {"message": "unauthorized"}
+    try:
+        is_admin(get_jwt())
+        service = update_service(data, service_id)
+        return jsonify(data=service), HTTPStatus.OK
+    except InvalidKeysError as e:
+        return jsonify(e.message), HTTPStatus.BAD_REQUEST
 
 
 @bp.delete("/<int:id>")
 @jwt_required()
 def delete_service_by_id(id):
-    current_service_id = get_jwt_identity()
-    session = current_app.db.session
-    if current_service_id == id:
-        try:
-            service = ServicesModel.query.get(current_service_id)
-            session.delete(service)
-            session.commit()
-            return {"message":"deleted"}, 404
-        except:
-            return {"message":"Not Found"}, 404
-    return {"message": "unauthorized"}
+    try:
+        is_admin(get_jwt())
+        return delete_service(), HTTPStatus.NO_CONTENT
+    except NotFoundError as e:
+        return jsonify(e.message), HTTPStatus.NOT_FOUND
