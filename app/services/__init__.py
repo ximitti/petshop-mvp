05255dcf56e735adec8596e8f5b21bd400377446
Diego Silva <dev.diegosilva@hotmail.com>
2021-07-20 13:44:56 -0300

    refactor: :recycle: Refactor Client Views
    refactored client views and client services

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -7,10 +7,5 @@ from app.services.petshop_service import (
     get_petshop_by_id,
 )
 from app.services.client_service import (
-    create_client,
-    get_clients,
-    get_token,
-    get_client_by_id,
-    update_client,
-    delete_client
-)
+        ClientServices
+ )
