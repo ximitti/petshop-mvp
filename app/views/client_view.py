df5c93c50e2ffe70b8196682c8681b3ca21b8a79
Diego Silva <dev.diegosilva@hotmail.com>
2021-07-21 10:21:06 -0300

    fix: :ambulance: Admin Verification and Key Verification
    Refactor client services and fix some bugs on client_view

--- app/views/client_view.py
+++ app/views/client_view.py
@@ -1,6 +1,9 @@
+from app.exc.status_option import InvalidKeysError
+from app.exc.status_unauthorized import Unauthorized
 from app.exc.status_not_found import NotFoundError
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import  get_jwt_identity, jwt_required
+from flask_jwt_extended import  get_jwt_identity, jwt_required,get_jwt
+from app.services.helpers import is_admin
 from http import HTTPStatus
 
 from app.services import ClientServices
@@ -12,32 +15,45 @@ bp  = Blueprint("bp_client", __name__, url_prefix="/api")
 
 
 @bp.get("/clients")
+@jwt_required()
 def get_clients():
     try:
+        is_admin(get_jwt())
         return jsonify({"data":ClientServices.get_clients()}),HTTPStatus.OK
     except NotFoundError as e:
         return jsonify(e.message), HTTPStatus.NOT_FOUND
+    except Unauthorized as e:
+        return jsonify(e.message), HTTPStatus.UNAUTHORIZED
 
 @bp.post("/clients/register")
 def create_client():
-    data = request.get_json()
-    return jsonify({"data":ClientServices.create_client(data).serialize}), HTTPStatus.CREATED
+    try:
+        data = request.get_json()
+        return jsonify({"data":ClientServices.create_client(data).serialize}), HTTPStatus.CREATED
+    except InvalidKeysError as e:
+        return jsonify(e.message), HTTPStatus.BAD_REQUEST
 
 @bp.post("/clients/login")
 def login():
-    data = request.get_json()
-    return jsonify({"data":{"access":ClientServices.get_token(data)}}), HTTPStatus.OK
+    try:
+        data = request.get_json()
+        return jsonify({"data":{"access":ClientServices.get_token(data)}}), HTTPStatus.OK
+    except InvalidKeysError as e:
+        return jsonify(e.message), HTTPStatus.BAD_REQUEST
 
 @bp.get("/clients/<int:id>")
 @jwt_required()
 def get_client_by_id(id):
     current_user_id = get_jwt_identity()
     try:
-        if current_user_id == id:
+        if current_user_id == id or is_admin(get_jwt()):
             return jsonify({"data":ClientServices.get_client_by_id(id)})
+        else:
+            raise Unauthorized
     except NotFoundError as e:
         return jsonify(e.message), HTTPStatus.NOT_FOUND
-    #except unautorized:
+    except Unauthorized as e:
+        return jsonify(e.message), HTTPStatus.UNAUTHORIZED
 
 @bp.patch("/clients/<int:id>")
 @jwt_required()
@@ -45,11 +61,16 @@ def update_client_by_id(id):
     try:
         current_user_id = get_jwt_identity()
         data = request.get_json()
-        if current_user_id == id:
-            return jsonify(ClientServices.update_client(data, id).serialize)
+        if current_user_id == id or is_admin(get_jwt()):
+            return jsonify(ClientServices.update_client(data, id))
+        else:
+            raise Unauthorized
+    except InvalidKeysError as e:
+        return jsonify(e.message), HTTPStatus.BAD_REQUEST
     except NotFoundError as e:
         return jsonify(e.message), HTTPStatus.NOT_FOUND
-    #except unautorized:
+    except Unauthorized as e:
+        return jsonify(e.message), HTTPStatus.UNAUTHORIZED
 
 
 @bp.delete("/clients/<int:id>")
@@ -57,11 +78,14 @@ def update_client_by_id(id):
 def delete_client_by_id(id):
     try:
         current_user_id = get_jwt_identity()
-        if current_user_id == id:
+        if current_user_id == id or is_admin(get_jwt()):
             return ClientServices.delete_client(id), HTTPStatus.NO_CONTENT
+        else:
+            raise Unauthorized
     except NotFoundError as e:
         return jsonify(e.message), HTTPStatus.NOT_FOUND
-    #except unautorized:
+    except Unauthorized as e:
+        return jsonify(e.message), HTTPStatus.UNAUTHORIZED
 
 @bp.post("/clients/<int:id>/address")
 @jwt_required()
@@ -86,7 +110,6 @@ def get_address(id):
         return {"error": "already exists"}, HTTPStatus.NOT_ACCEPTABLE        
     except NotFoundError as e:
         return jsonify(e.message), HTTPStatus.NOT_FOUND
-    #except unautorized:
 
 @bp.patch("/clients/<int:id>/address/<int:add_id>")
 @jwt_required()
@@ -96,11 +119,12 @@ def update_address(id, add_id):
         data = request.get_json()
         if current_user_id == id:
             return jsonify(ClientServices.updade_address_by_id(data, add_id).serialize), HTTPStatus.OK
+    except InvalidKeysError as e:
+        return jsonify(e.message), HTTPStatus.BAD_REQUEST
     except IntegrityError as _:
         return {"error": "already exists"}, HTTPStatus.NOT_ACCEPTABLE
     except NotFoundError as e:
         return jsonify(e.message), HTTPStatus.NOT_FOUND
-    #except unautorized:
 
 @bp.delete("/clients/<int:id>/address/<int:add_id>")
 @jwt_required()
@@ -111,4 +135,3 @@ def delete_edit_address(id, add_id):
             return jsonify(ClientServices.delete_address_by_id(add_id)), HTTPStatus.NO_CONTENT
     except NotFoundError as e:
         return jsonify(e.message), HTTPStatus.NOT_FOUND
-    #except unautorized:
